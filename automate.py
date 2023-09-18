10a9436ef0aa7f0e60179f1ddaa23bbd8387d605
AbhinavPandey1911 <abhinavpandeyjee@gmail.com>
2023-09-18 11:05:10 +0530

    added solutions

--- automate.py
+++ automate.py
@@ -2,4 +2,8 @@ import os
 
 def run():
     os.system('echo "pushing to github"')
-    
+    os.system('git add .')
+    os.system('git commit -m "added solutions"')
+    os.system('git push -u origin main')
+    print("Success")
+run()
